3e852eb039a250c237fb8c779a8b28afa8ec9d3b
ianstalter123 <istalter66@gmail.com>
2015-05-03 14:11:34 -0700

    attempt at underscore exercise

--- underscore.js
+++ underscore.js
@@ -1,1 +1,162 @@
-// PUT YOUR CODE HERE!!!
+	var _ = (function() {
+	     return {
+	         average: function (data) { 
+
+	         var i = 0;
+	         var total = 0;
+	         while (i<data.length)
+	         	{
+	         	total += data[i];
+	         	i++;
+	         	}
+	         return (total/data.length);
+
+	         	},
+	         contains: function(data,value) { 
+
+	         var i = 0;
+	         
+	         while (i<data.length)
+	         	{
+	         	if (data[i] === value)
+	         	{
+	         		return true;
+	         	}
+
+	         	i++;
+	         	}
+	         return false;
+
+	        	 },
+	         first: function (data) {
+
+	         	return data[0];
+	          	},
+
+	         last: function(data) { 
+	         	return data[data.length-1];
+	        	 },
+	         max: function (data) { 
+
+	         var i = 0;
+	         var max = 0;
+	         while (i<data.length)
+	         	{
+	         	if (data[i] > max)
+	         	max = data[i];
+	         	i++;
+	         	}
+	         return (max);
+
+	         	},
+	         min: function(data) { 
+
+	         var i = 0;
+	         var min = data[0];
+	         while (i<data.length)
+	        	 {
+	         	if (data[i] < min)
+	         	min = data[i];
+	         	i++;
+	         	}
+	         return (min);
+
+	        	 },
+	         shuffle: function (data) { 
+
+	         	var result = [];
+	         	while(data.length > 0)
+	         	{
+			 		var rand = Math.floor((Math.random()*data.length));
+			 	//console.log(rand);
+			 		result.push(data[rand]);
+			 		data.splice(rand,1)
+				}
+	         return result;
+	            },	         
+
+	         sample: function( data, value) {
+
+	         value= value || 1;	
+	         var result = [];
+
+	         while(value > 0)
+	         	{
+			 	var rand = Math.floor((Math.random()*data.length));
+			 	result.push( data[rand]);
+			 //console.log(rand);
+			 	value--;
+
+				}
+	         return (result);
+	          },
+
+	         difference: function (arr1, arr2) 
+	         {
+	         	var i = 0;
+	         	while (i < arr1.length)
+	         	{
+
+	         		var j = 0;
+	         		while (j < arr2.length)
+	         		{
+
+	         			if (arr1[i] == arr2[j])
+	         			{
+	         			    arr1.splice(i,1);
+	         			}
+	         		j++;
+	         		}
+
+	         		i++;
+
+	         	}
+	         	return arr1;
+	          },
+	         indexof: function(data,value) 
+	         { 
+	         	var i = 0;
+	         	var index = 0;
+	        
+	         	while (i<data.length)
+	        	 {
+	         		if (data[i] === value)
+	     			{
+	         			index = i;
+	         			return index;
+	     			}
+	     			else
+	     			{
+	     				index = -1;
+	     			}
+	         		i++;
+	         	}
+	         			return index; 
+	     		},
+
+
+	          pluck: function (obj,prop)
+	          { 
+	          		var i = 0;
+	         		var results = [];
+	        
+	         	   for(item in obj)
+	        	    { 		
+	        	 	results.push(obj[item].name);
+	         		i++;
+	     		  }
+	     		return results;
+
+	            },
+	      
+	      };
+
+	})();
+
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+	//console.log( _.max([1,2,3,4,5,10]));
+	//console.log(_.sample([1, 2, 3, 4, 5, 6]));
+	console.log(_.pluck(stooges, 'name'));
+	//console.log(_.shuffle([1, 2, 3, 4, 5, 6]));
+
+
